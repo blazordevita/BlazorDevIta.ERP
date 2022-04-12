962ddc936785eabe342d79d43f94760168bd78af
apomic80 <apomic80@hotmail.com>
2022-04-12 22:39:08 +0200

    generic grid

--- BlazorDevIta.Shared/WeatherForecastListItem.cs
+++ BlazorDevIta.Shared/WeatherForecastListItem.cs
@@ -1,12 +1,21 @@
+using BlazorDevIta.ERP.Infrastructure.Attributes;
 using System.ComponentModel.DataAnnotations;
 
 namespace BlazorDevIta.ERP.Shared
 {
     public class WeatherForecastListItem
     {
+        [Hidden]
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
         public DateTime Date { get; set; }
+
+        [Display(Name = "Temp (C)")]
         public int TemperatureC { get; set; }
+
+        [Hidden]
+        [Display(Name = "Temp (F)")]
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
 }
