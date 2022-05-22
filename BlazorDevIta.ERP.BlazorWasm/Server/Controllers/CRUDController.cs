892bf4758b791b78635f98dd04f207f5a1d53939
Volsoft73 <sandro.lomazzo@hotmail.it>
2022-05-22 18:05:23 +0200

    Three examples for check name of property on OrderBy* method

--- BlazorDevIta.ERP.BlazorWasm/Server/Controllers/CRUDController.cs
+++ BlazorDevIta.ERP.BlazorWasm/Server/Controllers/CRUDController.cs
@@ -1,4 +1,4 @@
-using AutoMapper;
+﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using BlazorDevIta.ERP.Infrastructure;
 using BlazorDevIta.ERP.Infrastructure.DataTypes;
@@ -9,172 +9,201 @@ using System.Linq.Expressions;
 
 namespace BlazorDevIta.ERP.BlazorWasm.Server.Controllers
 {
-	[ApiController]
-	// [Route("[controller]")]
-	public class CRUDController<ListItemType, DetailsType, IdType, EntityType> 
-		: ControllerBase
-		where ListItemType : BaseListItem<IdType>
-		where DetailsType : BaseDetails<IdType>
-		where EntityType : class, IEntity<IdType>, new()
-	{
+    [ApiController]
+    // [Route("[controller]")]
+    public class CRUDController<ListItemType, DetailsType, IdType, EntityType>
+        : ControllerBase
+        where ListItemType : BaseListItem<IdType>
+        where DetailsType : BaseDetails<IdType>
+        where EntityType : class, IEntity<IdType>, new()
+    {
         protected readonly IMapper _mapper;
         protected readonly ILogger<CRUDController<ListItemType, DetailsType, IdType, EntityType>> _logger;
-		protected readonly IRepository<EntityType, IdType> _repository;
+        protected readonly IRepository<EntityType, IdType> _repository;
 
-		protected readonly int pageSize = 2;
+        protected readonly int pageSize = 2;
 
-		public CRUDController(
-			IMapper mapper,
-			IRepository<EntityType, IdType> repository,
-			ILogger<CRUDController<ListItemType, DetailsType, IdType, EntityType>> logger)
-		{
-			_mapper = mapper;
-			_logger = logger;
-			_repository = repository;
-		}
+        public CRUDController(
+            IMapper mapper,
+            IRepository<EntityType, IdType> repository,
+            ILogger<CRUDController<ListItemType, DetailsType, IdType, EntityType>> logger)
+        {
+            _mapper = mapper;
+            _logger = logger;
+            _repository = repository;
+        }
 
-		[HttpDelete("{id}")]
-		public virtual async Task<IActionResult> Delete(IdType id)
-		{
-			var entity = await _repository.GetByIdAsync(id);
-			if (entity == null)
-			{
-				return NotFound();
-			}
-
-			await _repository.DeleteAsync(id);
-			return NoContent();
-		}
-
-		protected virtual Expression<Func<EntityType, bool>>? Filter(string filterText)
-			=> null;
-
-		[HttpGet]
-		[ProducesResponseType(StatusCodes.Status200OK)]
-		[ProducesDefaultResponseType]
-		public virtual async Task<IActionResult> Get(
-			[FromQuery]PageParameters parameters)
-		{
-			var result = _repository.GetAll();
-
-			if(!string.IsNullOrEmpty(parameters.FilterText))
+        [HttpDelete("{id}")]
+        public virtual async Task<IActionResult> Delete(IdType id)
+        {
+            var entity = await _repository.GetByIdAsync(id);
+            if (entity == null)
             {
-				var predicate = Filter(parameters.FilterText);
-				if (predicate != null)
-				{
-					result = result.Where(predicate);
-				}
+                return NotFound();
             }
-			
 
-			int itemCount = result.Count();
-			int pageCount = (itemCount + pageSize - 1) / pageSize;
-			if(parameters.Page > pageCount) parameters.Page = pageCount;
-			if(parameters.Page < 1) parameters.Page = 1;
+            await _repository.DeleteAsync(id);
+            return NoContent();
+        }
 
-			if(!string.IsNullOrEmpty(parameters.OrderBy))
+        protected virtual Expression<Func<EntityType, bool>>? Filter(string filterText)
+            => null;
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public virtual async Task<IActionResult> Get(
+            [FromQuery] PageParameters parameters)
+        {
+            var result = _repository.GetAll();
+
+            if (!string.IsNullOrEmpty(parameters.FilterText))
             {
-				try
-				{
-					if (parameters.OrderByDirection == OrderDirection.Ascendent)
-					{
-						result = result.OrderByProperty(parameters.OrderBy);
-					}
-					else
-					{
-						result = result.OrderByPropertyDescending(parameters.OrderBy);
-					}
-				}
-				catch
+                var predicate = Filter(parameters.FilterText);
+                if (predicate != null)
                 {
-					parameters.OrderBy = null;
-					parameters.OrderByDirection = OrderDirection.Ascendent;
+                    result = result.Where(predicate);
                 }
             }
 
-			var page = new Page<ListItemType, IdType>()
-			{
-				CurrentPage = parameters.Page,
-				ItemCount = itemCount,
-				PageCount = pageCount,
-				OrderBy = parameters.OrderBy,
-				OrderByDirection = parameters.OrderByDirection,
-				Items = await result
-					.Skip((parameters.Page - 1) * pageSize)
-					.Take(pageSize)
-					.ProjectTo<ListItemType>(_mapper.ConfigurationProvider)
-					.ToListAsync()
-			};
 
-			return Ok(page);
-		}
+            int itemCount = result.Count();
+            int pageCount = (itemCount + pageSize - 1) / pageSize;
+            if (parameters.Page > pageCount) parameters.Page = pageCount;
+            if (parameters.Page < 1) parameters.Page = 1;
 
-		[HttpGet("{id}")]
-		[ProducesDefaultResponseType]
-		public virtual async Task<IActionResult> GetById(IdType id)
-		{
-			var entity = await _repository.GetByIdAsync(id);
-			if (entity == null)
-			{
-				return NotFound();
-			}
+            if (!string.IsNullOrEmpty(parameters.OrderBy))
+            {
+                //local void
+                void Reset() { parameters.OrderBy = null; parameters.OrderByDirection = OrderDirection.Ascendent; }
 
-			var result = _mapper.Map<DetailsType>(entity);
-				
-				/*new WeatherForecastDetails()
-			{
-				Id = entity.Id,
-				Date = entity.Date,
-				TemperatureC = entity.TemperatureC,
-				Summary = entity.Summary
-			};*/
 
-			return Ok(result);
-		}
+                // ***First approach with action 👍 -------------------***
 
-		[HttpPost]
-		public virtual async Task<IActionResult> Post(DetailsType model)
-		{
-			if (ModelState.IsValid)
-			{
-				var entity = _mapper.Map<EntityType>(model);
-					/*new WeatherForecast()
-				{
-					Id = model.Id,
-					Date = model.Date,
-					TemperatureC = model.TemperatureC,
-					Summary = model.Summary
-				};*/
-				await _repository.CreateAsync(entity);
+                result = result.OrderByProperty(parameters.OrderBy, parameters.OrderByDirection, Reset);
 
-				model.Id = entity.Id;
+                // ***End First approach ------------------------------***
 
-				return CreatedAtAction(nameof(GetById), new { id = model.Id }, model);
-			}
-			return BadRequest(model);
-		}
 
-		[HttpPut("{id}")]
-		public virtual async Task<IActionResult> Put(IdType id, DetailsType model)
-		{
-			var entity = await _repository.GetByIdAsync(id);
-			if (entity == null)
-			{
-				return NotFound();
-			}
 
-			if (ModelState.IsValid)
-			{
-				entity = _mapper.Map<EntityType>(model);
 
-				/*entity.Date = model.Date;
+                // ***Second approach with wrapped object 🤔-------------------***
+
+                //var orderedValues = result.GetOrderedValues(parameters.OrderBy, parameters.OrderByDirection);
+
+                //if (!orderedValues.Error)
+                //{
+                //    result = orderedValues.value;
+                //}
+                //else Reset();
+
+
+                // ***End Second approach with warpped object 🤔---------------***
+
+
+
+
+
+                // ***Thrird approach Try and out parameter 🤔-------------------***
+
+                //if (result.TryOrderByProperty(parameters.OrderBy, parameters.OrderByDirection, out IQueryable<EntityType> queryable))
+                //{
+                //    result = queryable;
+                //}
+                //else Reset();
+
+
+                // ***End Thrird approach with warpped object 🤔-------------------***
+
+
+            }
+
+            var page = new Page<ListItemType, IdType>()
+            {
+                CurrentPage = parameters.Page,
+                ItemCount = itemCount,
+                PageCount = pageCount,
+                OrderBy = parameters.OrderBy,
+                OrderByDirection = parameters.OrderByDirection,
+                Items = await result
+                    .Skip((parameters.Page - 1) * pageSize)
+                    .Take(pageSize)
+                    .ProjectTo<ListItemType>(_mapper.ConfigurationProvider)
+                    .ToListAsync()
+            };
+
+            return Ok(page);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesDefaultResponseType]
+        public virtual async Task<IActionResult> GetById(IdType id)
+        {
+            var entity = await _repository.GetByIdAsync(id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            var result = _mapper.Map<DetailsType>(entity);
+
+            /*new WeatherForecastDetails()
+        {
+            Id = entity.Id,
+            Date = entity.Date,
+            TemperatureC = entity.TemperatureC,
+            Summary = entity.Summary
+        };*/
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public virtual async Task<IActionResult> Post(DetailsType model)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = _mapper.Map<EntityType>(model);
+                /*new WeatherForecast()
+            {
+                Id = model.Id,
+                Date = model.Date,
+                TemperatureC = model.TemperatureC,
+                Summary = model.Summary
+            };*/
+                await _repository.CreateAsync(entity);
+
+                model.Id = entity.Id;
+
+                return CreatedAtAction(nameof(GetById), new { id = model.Id }, model);
+            }
+            return BadRequest(model);
+        }
+
+        [HttpPut("{id}")]
+        public virtual async Task<IActionResult> Put(IdType id, DetailsType model)
+        {
+            var entity = await _repository.GetByIdAsync(id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                entity = _mapper.Map<EntityType>(model);
+
+                /*entity.Date = model.Date;
 				entity.TemperatureC = model.TemperatureC;
 				entity.Summary = model.Summary;*/
 
-				await _repository.UpdateAsync(entity);
-				return NoContent();
-			}
-			return BadRequest(model);
-		}
-	}
+                await _repository.UpdateAsync(entity);
+                return NoContent();
+            }
+            return BadRequest(model);
+        }
+
+
+
+    }
 }
--- BlazorDevIta.ERP.Infrastructure/Extensions/QueryableExtensions.cs
+++ BlazorDevIta.ERP.Infrastructure/Extensions/QueryableExtensions.cs
@@ -1,58 +1,177 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using BlazorDevIta.ERP.Infrastructure.Attributes;
+using BlazorDevIta.ERP.Infrastructure.DataTypes;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+
+
 
 namespace BlazorDevIta.ERP.Infrastructure.Extensions
 {
+
+
     public static class QueryableExtensions
     {
+
+        #region Private 
+
+
         private static readonly MethodInfo OrderByMethod = typeof(Queryable)
+                  .GetMethods()
+                  .Where(method => method.Name == nameof(Queryable.OrderBy))
+                  .Where(method => method.GetParameters().Length == 2)
+                  .Single();
+
+        private static readonly MethodInfo OrderByDescendingMethod = typeof(Queryable)
                    .GetMethods()
-                   .Where(method => method.Name == "OrderBy")
+                   .Where(method => method.Name == nameof(Queryable.OrderByDescending))
                    .Where(method => method.GetParameters().Length == 2)
                    .Single();
 
-        private static readonly MethodInfo OrderByDescendingMethod = typeof(Queryable)
-           .GetMethods()
-           .Where(method => method.Name == "OrderByDescending")
-           .Where(method => method.GetParameters().Length == 2)
-           .Single();
 
-        public static IQueryable<TSource> OrderByProperty<TSource>(this IQueryable<TSource> source, string propertyName)
+        private static object? GetMethodInvoke<TSource>(IQueryable<TSource> source, string propertyName, OrderDirection direction)
         {
+
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            if (string.IsNullOrWhiteSpace(propertyName)) return null;
+
+            var property = typeof(TSource).GetProperties()
+                .Where(p => p.Name == propertyName)
+                .Where(p => p.GetCustomAttribute<NotOrderableAttribute>() == null)
+                .SingleOrDefault();
+
+            if (property == null) return null;
+
             // x => x.<proprietà> (es. x => x.Date)
 
             // x
+
             ParameterExpression parameter = Expression.Parameter(typeof(TSource), "x");
-            
+
             // x.<propertyName>
-            Expression orderByProperty = Expression.Property(parameter, propertyName);
-            
+            Expression orderByProperty = Expression.Property(parameter, property);
+
             // x => x.<propertyName>
             LambdaExpression lambda = Expression.Lambda(orderByProperty, new[] { parameter });
 
-            MethodInfo genericMethod = OrderByMethod.MakeGenericMethod
-                (new[] { typeof(TSource), orderByProperty.Type });
+            MethodInfo genericMethod = direction == OrderDirection.Ascendent ? OrderByMethod.MakeGenericMethod
+                (new[] { typeof(TSource), orderByProperty.Type }) :
+                OrderByDescendingMethod.MakeGenericMethod(new[] { typeof(TSource), orderByProperty.Type });
+
 
             object? ret = genericMethod.Invoke(null, new object[] { source, lambda });
-            return ret != null ? (IQueryable<TSource>)ret : throw new Exception("invoke failed");
+
+            return ret;
         }
 
-        public static IQueryable<TSource> OrderByPropertyDescending<TSource>(this IQueryable<TSource> source, string propertyName)
+
+        #endregion
+
+
+        #region Methods with Action
+
+
+        public static IQueryable<TSource> OrderByPropertyAscending<TSource>(this IQueryable<TSource> source, string propertyName, Action reset)
         {
-            ParameterExpression parameter = Expression.Parameter(typeof(TSource), "x");
-            Expression orderByProperty = Expression.Property(parameter, propertyName);
-
-            LambdaExpression lambda = Expression.Lambda(orderByProperty, new[] { parameter });
-            MethodInfo genericMethod = OrderByDescendingMethod.MakeGenericMethod
-                (new[] { typeof(TSource), orderByProperty.Type });
-            object? ret = genericMethod.Invoke(null, new object[] { source, lambda });
-            return ret != null ? (IQueryable<TSource>)ret : throw new Exception("invoke failed");
+            return OrderByProperty(source, propertyName, OrderDirection.Ascendent, reset);
         }
+
+        public static IQueryable<TSource> OrderByPropertyDescending<TSource>(this IQueryable<TSource> source, string propertyName, Action reset)
+        {
+            return OrderByProperty(source, propertyName, OrderDirection.Descendent, reset);
+        }
+
+        public static IQueryable<TSource> OrderByProperty<TSource>(this IQueryable<TSource> source, string propertyName, OrderDirection direction, Action reset)
+        {
+            object? ret = GetMethodInvoke(source, propertyName, direction);
+
+            if (ret != null)
+            {
+                return (IQueryable<TSource>)ret;
+            }
+            else
+            {
+                reset?.Invoke();
+                return source;
+            }
+
+        }
+
+
+        #endregion
+
+
+        #region Methods with wrapped object
+
+
+        public static OrderedQueryable<TSource> GetOrderedValuesAscending<TSource>(this IQueryable<TSource> source, string propertyName)
+        {
+            return GetOrderedValues(source, propertyName, OrderDirection.Ascendent);
+        }
+
+        public static OrderedQueryable<TSource> GetOrderedValuesDescending<TSource>(this IQueryable<TSource> source, string propertyName)
+        {
+            return GetOrderedValues(source, propertyName, OrderDirection.Descendent);
+        }
+
+        public static OrderedQueryable<TSource> GetOrderedValues<TSource>(this IQueryable<TSource> source, string propertyName, OrderDirection direction)
+        {
+            object? ret = GetMethodInvoke(source, propertyName, direction);
+
+            if (ret != null)
+            {
+                return new OrderedQueryable<TSource>(false, (IQueryable<TSource>)ret);
+            }
+            else
+            {
+                return new OrderedQueryable<TSource>(true, source);
+            }
+
+        }
+
+
+        #endregion
+
+
+        #region Methods with Try & out parameter
+
+
+        public static bool TryOrderByPropertAscending<TSource>(this IQueryable<TSource> source, string propertyName, out IQueryable<TSource> queryable)
+        {
+            return TryOrderByProperty(source, propertyName, OrderDirection.Ascendent, out queryable);
+        }
+
+        public static bool TryOrderByPropertDescending<TSource>(this IQueryable<TSource> source, string propertyName, out IQueryable<TSource> queryable)
+        {
+            return TryOrderByProperty(source, propertyName, OrderDirection.Descendent, out queryable);
+        }
+
+        public static bool TryOrderByProperty<TSource>(this IQueryable<TSource> source, string propertyName, OrderDirection direction, out IQueryable<TSource> queryable)
+        {
+
+            object? ret = GetMethodInvoke(source, propertyName, direction);
+
+            if (ret != null)
+            {
+                queryable = (IQueryable<TSource>)ret;
+
+                return true;
+            }
+            else
+            {
+                queryable = null!;
+
+                return false;
+            }
+
+           
+
+        }
+
+
+        #endregion
+
+
     }
 }
 
