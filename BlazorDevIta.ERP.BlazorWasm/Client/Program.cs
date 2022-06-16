01fee0f788eb3856374df1a299e7a194afaf0b49
apomic80 <apomic80@hotmail.com>
2022-06-16 22:38:22 +0200

    issues 28 completed

--- BlazorDevIta.ERP.BlazorWasm/Client/Program.cs
+++ BlazorDevIta.ERP.BlazorWasm/Client/Program.cs
@@ -1,7 +1,7 @@
 using BlazorDevIta.ERP.BlazorWasm.Client.Services;
 using BlazorDevIta.UI;
+using BlazorDevIta.UI.Configuration;
 using BlazorDevIta.UI.Services;
-
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -12,5 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped(typeof(IDataServices<,,>), typeof(DataServices<,,>));
+
+builder.Services.AddBlazorDevItaUI();
 
 await builder.Build().RunAsync();
---
+++ BlazorDevIta.UI/Configuration/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using BlazorDevIta.UI.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorDevIta.UI.Configuration
+{
+    public static class Configuration
+    {
+        public static IServiceCollection AddBlazorDevItaUI(this IServiceCollection services)
+        {
+            services.AddSingleton<IConfirmService, ConfirmService>();
+            return services; ;
+        }
+    }
+}
